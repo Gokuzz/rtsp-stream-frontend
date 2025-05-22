5ceaa731b7e46f07f43e7cc06bd1d4ba24c77a07
viraj <virajshivaji.shedge@mail.ivtree.com>
2025-05-22 19:38:04 +0530

    fix

--- src/components/StreamPlayer.js
+++ src/components/StreamPlayer.js
@@ -13,7 +13,7 @@ const StreamPlayer = ({ stream, onRemove }) => {
             const player = mpegts.createPlayer({
                 type: 'mpegts',
                 isLive: true,
-                url: `ws://rtsp-stream-backend-1.onrender.com/ws/stream/${stream.id}/?rtsp=${rtspUrl}`,
+                url: `wss://rtsp-stream-backend-1.onrender.com/ws/stream/${stream.id}/?rtsp=${rtspUrl}`,
             }, {
                 enableWorker: true,
                 liveBufferLatencyChasing: true,
